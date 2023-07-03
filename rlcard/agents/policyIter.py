f7806769f0ae1884696daa746105284e0f6f7ef8
Artemijo5 <118560004+Artemijo5@users.noreply.github.com>
2023-07-03 13:10:15 +0300

    testing

--- rlcard/agents/policyIter.py
+++ rlcard/agents/policyIter.py
@@ -324,7 +324,7 @@ class PolicyIterator():
                                         chance = chance / (a_prob[1] + a_prob[2] + a_prob[3])
                                 # game lasts two rounds, so no next state
                                 V[player_id][raised+1][s] += chance*q
-                                print(V[player_id][raised+1][s])
+                                print('reward %d and chance %d', q, chance)
             if np.max(np.abs(V - prev_V)) < epsilon:
                 break
             prev_V = V.copy()
