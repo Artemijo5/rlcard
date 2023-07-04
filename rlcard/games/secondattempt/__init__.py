41e220ee3259cc7b507d105a43085954907bce4d
Artemijo5 <118560004+Artemijo5@users.noreply.github.com>
2023-07-04 19:50:36 +0300

    deleted unnecessary files

--- rlcard/games/secondattempt/__init__.py
+++
@@ -1,7 +0,0 @@
-from rlcard.games.secondattempt.dealer import LimitHoldemDealer as Dealer
-from rlcard.games.secondattempt.judger import LimitHoldemJudger as Judger
-from rlcard.games.secondattempt.player import LimitHoldemPlayer as Player
-from rlcard.games.secondattempt.player import PlayerStatus
-from rlcard.games.secondattempt.round import LimitHoldemRound as Round
-from rlcard.games.secondattempt.game import LimitHoldemGame as Game
-
--- rlcard/games/simplepoker/__init__.py
+++
@@ -1,6 +0,0 @@
-from rlcard.games.simplepoker.dealer import SimplePokerDealer as Dealer
-from rlcard.games.simplepoker.judger import SimplePokerJudger as Judger
-from rlcard.games.simplepoker.player import SimplePokerPlayer as Player
-from rlcard.games.simplepoker.player import PlayerStatus
-from rlcard.games.simplepoker.round import SimplePokerRound as Round
-from rlcard.games.simplepoker.game import SimplePokerGame as Game
